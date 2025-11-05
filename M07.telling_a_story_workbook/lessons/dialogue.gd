282b8219dfe2e6dd494ffecbf026d9ba4d0dedf2
aiielujah2 <ryzhen@s.sfusd.edu>
2025-11-05 13:36:30 -0800

    m7l10

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -1,36 +1,67 @@
 extends Control
 
+var expressions := {
+	"happy": preload ("res://assets/emotion_happy.png"),
+	"regular": preload ("res://assets/emotion_regular.png"),
+	"sad": preload ("res://assets/emotion_sad.png"),
+}
+
+
+var dialogue_items: Array[Dictionary] = [
+	{
+		"expression": expressions["regular"],
+		"text": "I'm learning about Arrays...",
+	},
+	{
+		"expression": expressions["sad"],
+		"text": "... and it is a little bit complicated.",
+	},
+	{
+		"expression": expressions["happy"],
+		"text": "Let's see if I got it right: an array is a list of values!",
+	},
+	{
+		"expression": expressions["regular"],
+		"text": "Did I get it right? Did I?",
+	},
+	{
+		"expression": expressions["happy"],
+		"text": "Hehe! Bye bye~!",
+	},
+]
+var current_item_index := 0
+
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 @onready var next_button: Button = %NextButton
 @onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
+@onready var body: TextureRect = %Body
+@onready var expression: TextureRect = %Expression
 
-
-var dialogue_items: Array[String] = [
-	"I'm learning about Arrays...",
-	"...and it is a little bit complicated.",
-	"Let's see if I got it right: an array is a list of values!",
-	"Did I get it right? Did I?",
-	"Hehe! Bye bye~!",
-]
-
-var current_item_index := 0
 
 func _ready() -> void:
 	show_text()
 	next_button.pressed.connect(advance)
 
 
+
 func show_text() -> void:
+
 	var current_item := dialogue_items[current_item_index]
-	rich_text_label.text = current_item
+	rich_text_label.text = current_item["text"]
+	expression.texture = current_item["expression"]
+	
 	rich_text_label.visible_ratio = 0.0
 	var tween := create_tween()
-	var text_appearing_duration :=1.2
+	var text_appearing_duration := 1.0
 	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
+
+	
 	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
 	var sound_start_position := randf() * sound_max_offset
 	audio_stream_player.play(sound_start_position)
 	tween.finished.connect(audio_stream_player.stop)
+
+
 func advance() -> void:
 	current_item_index += 1
 
